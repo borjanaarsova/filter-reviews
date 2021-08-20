83d86b19418b35c9e0e9fc3ec5a4cea632731215
borjanaarsova <39854810+borjanaarsova@users.noreply.github.com>
2021-08-20 19:03:41 +0200

    updated filter.php
    need to add filter for reviews with minimum review chosen, and a table for showing the sorted reviews

--- usb web server/root/New folder/filter.php
+++ usb web server/root/New folder/filter.php
@@ -5,179 +5,227 @@
 <center>
 <?php
 extract($_POST);
-$by_text=$_POST['by_text'];
-$by_rating=$_POST['by_rating'];
-$by_date=$_POST['by_date'];
-$rating_min=$_POST['rating_min'];
-/*If you return -1 that moves the $b variable down the array,
-return 1 moves $b up the array and
-return 0 keeps $b in the same place. 
-*/
+
 
 $json = file_get_contents('./reviews.json');
 $data = json_decode($json, true);
 
-usort($data,function /*sortReviews*/($a, $b) use ($by_text, $by_rating, $by_date){
+usort($data, function ($a, $b) use ($by_text, $by_rating, $by_date)
+{
 	if ($by_text=='Yes') //tekst prvo pred prazno review
 	{
-		if ($a['reviewText']!="" && $b['reviewText']!="")//ako dvete imat tekst
+		if(($a['reviewText']!="" && $b['reviewText']==""))//a e tekst, a b e prazno
+		{
+			return -1; //okej e
+		}
+		if (($a['reviewText']!="" && $b['reviewText']!="") || ($a['reviewText']=="" && $b['reviewText']==""))//ako dvete imat tekst, ili dvete se prazni
 		{
 			if($by_rating=="Highest First") //po najvisok rating
 			{
-				if ($a['rating'] == $b['rating'] || $a['rating']>$b['rating'])
+				if ($a['rating'] == $b['rating'])//ednakov rating
 				{
 					if($by_date=="Newest First") //najnovi prvo
 					{
-						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'] || $a['reviewCreatedOnDate']>$b['reviewCreatedOnDate'])
+						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'])//istovremeno kreirani
 						{
 							return 0; //okej podredeno
 						}
+						if($a['reviewCreatedOnDate']<$b['reviewCreatedOnDate']) //descending
+						{
+							return 1;
+						}
 						else
 						{
-							return -1; //b na levo
+							return -1;
 						}
 					}
 					else //najstari prvo
 					{
-						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'] || $a['reviewCreatedOnDate']<$b['reviewCreatedOnDate'])
+						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'])//istovremeno kreirani
 						{
 							return 0; //okej podredeno
 						}
+						if($a['reviewCreatedOnDate']<$b['reviewCreatedOnDate']) //ascending
+						{
+							return -1;
+						}
 						else
 						{
-							return -1; //b na levo
+							return 1;
 						}
 					}
 				}
-				else
+				else //razlicen rating, sporedi gi dvata pa podredi po descending
 				{
-					return -1; //b na levo
+					if($a['rating']<$b['rating'])
+					{
+						return 1;
+					}
+					else
+					{
+						return -1;
+					}
 				}
 			}
 			else //po najnizok rejting
 			{
-				if ($a['rating'] == $b['rating'] || $a['rating']<$b['rating'])
+				if ($a['rating'] == $b['rating'])//ednakov rating
 				{
 					if($by_date=="Newest First") //najnovi prvo
 					{
-						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'] || $a['reviewCreatedOnDate']>$b['reviewCreatedOnDate'])
+						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'])//istovremeno kreirani
 						{
 							return 0; //okej podredeno
 						}
+						if($a['reviewCreatedOnDate']<$b['reviewCreatedOnDate']) //descending
+						{
+							return 1;
+						}
 						else
 						{
-							return -1; //b na levo
+							return -1;
 						}
 					}
 					else //najstari prvo
 					{
-						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'] || $a['reviewCreatedOnDate']<$b['reviewCreatedOnDate'])
+						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'])//istovremeno kreirani
 						{
 							return 0; //okej podredeno
 						}
+						if($a['reviewCreatedOnDate']<$b['reviewCreatedOnDate']) //ascending
+						{
+							return -1;
+						}
 						else
 						{
-							return -1; //b na levo
+							return 1;
 						}
 					}
 				}
-				else
+				else //razlicen rating, sporedi gi dvata pa podredi po ascending
 				{
-					return -1; //b na levo
+					if($a['rating']<$b['rating'])
+					{
+						return -1;
+					}
+					else
+					{
+						return 1;
+					}
 				}
 			}
 		}
-		else //ako nekoe e prazno
-		{
-			if($a['reviewText']=="" && $b['reviewText']!="")//ako a e prazno a b e so tekst
-			{
-				return -1; //b na levo
-			}
-			else
-			{
-				return 0; //b ostanuva na mesto
-			}
+		else //ako a e prazno, a b e tekst, treba descending
+		{			
+			return 1; //b na levo
 		}
-    }
-	else //prazno pred tekst
+	}
+	else //prazno review pred tekst
 	{
-		if ($a['reviewText']=="" && $b['reviewText']=="") //ako dvete se prazni
+		if(($a['reviewText']=="" && $b['reviewText']!=""))//a e prazno, a b e tekst
+		{
+			return -1; //okej e
+		}
+		if (($a['reviewText']!="" && $b['reviewText']!="") || ($a['reviewText']=="" && $b['reviewText']==""))//ako dvete imat tekst, ili dvete se prazni
 		{
 			if($by_rating=="Highest First") //po najvisok rating
 			{
-				if ($a['rating'] == $b['rating'] || $a['rating']>$b['rating'])
+				if ($a['rating'] == $b['rating'])//ednakov rating
 				{
 					if($by_date=="Newest First") //najnovi prvo
 					{
-						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'] || $a['reviewCreatedOnDate']>$b['reviewCreatedOnDate'])
+						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'])//istovremeno kreirani
 						{
 							return 0; //okej podredeno
 						}
+						if($a['reviewCreatedOnDate']<$b['reviewCreatedOnDate']) //descending
+						{
+							return 1;
+						}
 						else
 						{
-							return -1; //b na levo
+							return -1;
 						}
 					}
 					else //najstari prvo
 					{
-						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'] || $a['reviewCreatedOnDate']<$b['reviewCreatedOnDate'])
+						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'])//istovremeno kreirani
 						{
 							return 0; //okej podredeno
 						}
+						if($a['reviewCreatedOnDate']<$b['reviewCreatedOnDate']) //ascending
+						{
+							return -1;
+						}
 						else
 						{
-							return -1; //b na levo
+							return 1;
 						}
 					}
 				}
-				else
+				else //razlicen rating, sporedi gi dvata pa podredi po descending
 				{
-					return -1; //b na levo
+					if($a['rating']<$b['rating'])
+					{
+						return 1;
+					}
+					else
+					{
+						return -1;
+					}
 				}
 			}
 			else //po najnizok rejting
 			{
-				if ($a['rating'] == $b['rating'] || $a['rating']<$b['rating'])
+				if ($a['rating'] == $b['rating'])//ednakov rating
 				{
 					if($by_date=="Newest First") //najnovi prvo
 					{
-						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'] || $a['reviewCreatedOnDate']>$b['reviewCreatedOnDate'])
+						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'])//istovremeno kreirani
 						{
 							return 0; //okej podredeno
 						}
+						if($a['reviewCreatedOnDate']<$b['reviewCreatedOnDate']) //descending
+						{
+							return 1;
+						}
 						else
 						{
-							return -1; //b na levo
+							return -1;
 						}
 					}
 					else //najstari prvo
 					{
-						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'] || $a['reviewCreatedOnDate']<$b['reviewCreatedOnDate'])
+						if ($a['reviewCreatedOnDate'] == $b['reviewCreatedOnDate'])//istovremeno kreirani
 						{
 							return 0; //okej podredeno
 						}
+						if($a['reviewCreatedOnDate']<$b['reviewCreatedOnDate']) //ascending
+						{
+							return -1;
+						}
 						else
 						{
-							return -1; //b na levo
+							return 1;
 						}
 					}
 				}
-				else
+				else //razlicen rating, sporedi gi dvata pa podredi po ascending
 				{
-					return -1; //b na levo
+					if($a['rating']<$b['rating'])
+					{
+						return -1;
+					}
+					else
+					{
+						return 1;
+					}
 				}
 			}
 		}
-		else //ako nekoe e polno
-		{
-			if($a['reviewText']!="" && $b['reviewText']=="")//ako a e polno a b e prazno
-			{
-				return -1; //b na levo
-			}
-			else
-			{
-				return 0; //b ostanuva na mesto
-			}
+		else //ako a e tekst, a b e prazno, treba ascending
+		{			
+			return 1; //b na levo
 		}
 	}
 });
@@ -191,7 +239,7 @@ usort($data,function /*sortReviews*/($a, $b) use ($by_text, $by_rating, $by_date
     //}
 
 //echo $data->"0";
-	//print_r($data);
+	print_r($data);
 	//echo $data;
 	//var_dump($data);
 
